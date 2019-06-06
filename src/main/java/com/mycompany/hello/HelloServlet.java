132a6711412b69c3158eed409d819adffa5b5015
Jason A. Hurst <hurst_jason@bah.com>
2019-06-06 14:58:18 -0400

    fixing things

--- src/main/java/com/mycompany/hello/HelloServlet.java
+++ src/main/java/com/mycompany/hello/HelloServlet.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
+    public String hello_str = "Hello Servlet";
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
@@ -15,7 +16,7 @@ public class HelloServlet extends HttpServlet {
         out.println("<TITLE>Hello Servlet</TITLE>");
         out.println("</HEAD>");
         out.println("<BODY>");
-        out.println("Hello Servlet");
+        out.println(hello_str);
         out.println("</BODY>");
         out.println("</HTML>");
     }
